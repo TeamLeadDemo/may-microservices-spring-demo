bf512b3b5487027896e5d2fa9c89d53b0bb5108e
DeveloperOneDemo <92218513+DeveloperOneDemo@users.noreply.github.com>
2022-06-22 12:30:03 -0500

    microservices-completed

---
+++ departments-service/src/test/java/com/demo/departmentsservice/DepartmentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.departmentsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ employees-service/src/main/java/com/demo/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.demo.employeeservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.employeeservice.entity.Employee;
+import com.demo.employeeservice.service.EmployeeService;
+import com.demo.employeeservice.vo.ResEmpDept;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/employees")
+@Slf4j
+public class EmployeeController {
+
+	@Autowired
+	EmployeeService employeeService;
+	
+	// add an employee
+	@PostMapping("/")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		log.info("Entering addEmployee of EmployeeController...");
+		Employee returnedEmployee = employeeService.addEmployee(employee);
+		return returnedEmployee;
+	}
+	
+	
+	// fetch an employee
+	@GetMapping("/{eid}")
+	public ResEmpDept getAEmployee(@PathVariable("eid") int employeeId) {
+		log.info("Entering getAEmployee of EmployeeController...");
+		ResEmpDept red = employeeService.getAEmployee(employeeId);
+		return red;
+		
+	}
+	
+}
